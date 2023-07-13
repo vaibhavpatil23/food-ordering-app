dbcc695fd294ac0f1bbbc874701aa06b88b08151
vaibhav <vpatil7382@gmail.com>
2023-07-13 20:40:25 +0530

    initial commit

--- src/componant/Header.js
+++ src/componant/Header.js
@@ -1,17 +1,20 @@
 import { LOGO_URL } from "../utils/constant";
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import useOnlineStatus from "../utils/useonlinestatus";
 
 const Header = () => {
   const [btnNameReact, setBtnNameReact] = useState("login");
 
+  const onlineStatus = useOnlineStatus()
   return (
     <div className="header">
       <div className="logo-container">
         <img className="logo" src={LOGO_URL} alt="" />
       </div>
-      <div className="nav-item">
+      <div className="nav-item">		✅	✅✅
         <ul>
+          <li>Online Status : {onlineStatus ? "Right" : "Wrong"}</li>
           <li> <Link to="/">Home</Link></li>
           <li><Link to="about">About</Link></li>
           <li> <Link to="contact">Contact Us</Link></li>
