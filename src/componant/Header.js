fb1b904a2819d0ee1d008b6efc1e6125397a74a0
vaibhav <vpatil7382@gmail.com>
2023-07-14 11:41:06 +0530

    initial commit

--- src/componant/Header.js
+++ src/componant/Header.js
@@ -6,19 +6,30 @@ import useOnlineStatus from "../utils/useonlinestatus";
 const Header = () => {
   const [btnNameReact, setBtnNameReact] = useState("login");
 
-  const onlineStatus = useOnlineStatus()
+  const onlineStatus = useOnlineStatus();
   return (
     <div className="header">
       <div className="logo-container">
         <img className="logo" src={LOGO_URL} alt="" />
       </div>
-      <div className="nav-item">		✅	✅✅
+      <div className="nav-item">
         <ul>
-          <li>Online Status : {onlineStatus ? "Right" : "Wrong"}</li>
-          <li> <Link to="/">Home</Link></li>
-          <li><Link to="about">About</Link></li>
-          <li> <Link to="contact">Contact Us</Link></li>
-          <li> <Link to="Cards">Cards</Link></li>
+          <li>Online Status : {onlineStatus ? "		✅" : "	🔴"}</li>
+          <li>
+            {" "}
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="about">About</Link>
+          </li>
+          <li>
+            {" "}
+            <Link to="contact">Contact Us</Link>
+          </li>
+          <li>
+            {" "}
+            <Link to="Cards">Cards</Link>
+          </li>
           <button
             className="login"
             onClick={() => {
