7e3a4a75e001be324fa96af2b9578eebe4ea18f7
vaibhav <vpatil7382@gmail.com>
2023-07-26 18:26:28 +0530

    initial commit

--- src/componant/Cart.js
+++ src/componant/Cart.js
@@ -4,23 +4,27 @@ import { clearCart } from "../utils/CartSlice";
 import { useDispatch } from "react-redux";
 
 const Cart = () => {
+  const CartItems = useSelector((store) => store.Cart.items);
 
-const CartItems = useSelector((store) => store.Cart.items)
- 
-const dispatch = useDispatch()
-const handelClearcart = () => {
-dispatch(clearCart())
-}
-
-  return <div className="text-center m-4 p-4">
-    <h1 className="text-2xl font-bold">Cart</h1>
-    <div className="w-6/12 m-auto">
-        <button className="p-2 m-2 bg-black text-white rounded-lg" onClick={
-            handelClearcart
-        }>Clear Cart</button>
+  const dispatch = useDispatch();
+  const handelClearcart = () => {
+    dispatch(clearCart());
+  };
+  
+  return (
+    <div className="text-center m-4 p-4">
+      <h1 className="text-2xl font-bold">Cart</h1>
+      <div className="w-6/12 m-auto">
+        <button
+          className="p-2 m-2 bg-black text-white rounded-lg"
+          onClick={handelClearcart}
+        >
+          Clear Cart
+        </button>
         {CartItems.length === 0 && <h1>Cart is Empty Add Items to The Cart</h1>}
-        <Itemlist items = {CartItems}/>
+        <Itemlist items={CartItems} />
+      </div>
     </div>
-  </div>;
+  );
 };
 export default Cart;
