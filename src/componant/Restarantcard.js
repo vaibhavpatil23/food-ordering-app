a57f849f770d14c36b546fca733583c1c00e0efa
vaibhav <vpatil7382@gmail.com>
2023-08-24 20:00:52 +0530

    minner change

--- src/componant/Restarantcard.js
+++ src/componant/Restarantcard.js
@@ -14,7 +14,7 @@ const RestaurentCard = (props) => {
     deliveryTime,
   } = resData?.data;
   return (
-    <div className="m-4 p-4 w-[270px]  h-[500px] bg-gray-100  rounded-lg hover:bg-gray-200 hover:border border-solid border-black hover:shadow-xl">
+    <div className="m-4 p-4 w-[272px]  h-[500px] bg-gray-100  rounded-lg hover:bg-gray-200 hover:border border-solid border-black hover:shadow-xl">
       <img className="rounded-lg" src={CDN_URL + cloudinaryImageId} />
       <h3 className="font-bold py-2 text-center">{name}</h3>
       <h4 className="font-bold py-2 text-center">{cuisines.join(" ,")}</h4>
